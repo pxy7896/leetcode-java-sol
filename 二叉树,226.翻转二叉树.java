fa8e36b72bf6d8b80622d9b632bf8ecace940e86
Ruriko Pang <2419817128@qq.com>
2019-12-30 15:41:03 +0800

    Create 二叉树,226.翻转二叉树.java

---
+++ 二叉树,226.翻转二叉树.java
@@ -0,0 +1,42 @@
+翻转一棵二叉树。
+
+示例：
+输入：
+     4
+   /   \
+  2     7
+ / \   / \
+1   3 6   9
+
+输出：
+     4
+   /   \
+  7     2
+ / \   / \
+9   6 3   1
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/invert-binary-tree
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+=================解法==============================
+先翻转当前节点的左右子节点，然后到下一层
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) return root;
+        TreeNode temp = root.left; 
+        root.left = root.right;
+        root.right = temp;
+        root.left = invertTree(root.left);
+        root.right = invertTree(root.right);
+        return root;
+    }
+}
