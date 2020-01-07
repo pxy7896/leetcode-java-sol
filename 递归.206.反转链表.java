9e953cf4853e71a7c8e6829f4790b1be3fded9f2
Ruriko Pang <2419817128@qq.com>
2020-01-07 09:08:20 +0800

    Create 递归.206.反转链表.java

---
+++ 递归.206.反转链表.java
@@ -0,0 +1,47 @@
+反转一个单链表。
+
+示例:
+输入: 1->2->3->4->5->NULL
+输出: 5->4->3->2->1->NULL
+
+进阶:
+你可以迭代或递归地反转链表。你能否用两种方法解决这道题？
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/reverse-linked-list
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+=======================解法===================================
+1.递归法
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null) return head;
+        ListNode cur = reverseList(head.next); // 此时递归到链表尾部节点
+        head.next.next = head;  // head是倒数第二个节点。
+        head.next = null;
+        return cur; // 需要一直跳到最后，所以要return cur
+    }
+}
+
+2.指针法
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode pre = null;
+        ListNode cur = head;
+        ListNode temp = null;
+        while (cur != null) {
+            temp = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = temp;
+        }
+        return pre;
+    }
+}
