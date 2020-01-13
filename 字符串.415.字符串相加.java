879bbe2030e9cd401a5d7f34d6399b2e3d926830
Ruriko Pang <2419817128@qq.com>
2020-01-13 09:25:35 +0800

    Update 字符串.415.字符串相加.java

--- 字符串.415.字符串相加.java
+++ 字符串.415.字符串相加.java
@@ -27,3 +27,37 @@ class Solution {
 		return s.reverse().toString();
     }
 }
+
+类似的一题
+989. 数组形式的整数加法
+对于非负整数 X 而言，X 的数组形式是每位数字按从左到右的顺序形成的数组。例如，如果 X = 1231，那么其数组形式为 [1,2,3,1]。
+给定非负整数 X 的数组形式 A，返回整数 X+K 的数组形式。
+示例 1：
+输入：A = [1,2,0,0], K = 34
+输出：[1,2,3,4]
+解释：1200 + 34 = 1234
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/add-to-array-form-of-integer
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+===================解法===========================
+这里需要额外考虑addToArrayForm([0], 23)这种原数组过短的情况，所以K和A必须分别跟carry相加
+class Solution {
+    public List<Integer> addToArrayForm(int[] A, int K) {
+        int carry = 0;
+        int i = A.length - 1;
+        List<Integer> result = new ArrayList<Integer>();
+        while (K != 0 || i >= 0 || carry != 0) {
+        	if (i >= 0)
+        		carry += A[i--]; 
+        	if (K != 0) {
+        		carry += K % 10;
+        		K /= 10;
+        	}
+        	result.add(carry % 10);
+        	carry /= 10;
+        }
+        Collections.reverse(result); // 逆序List
+        return result;
+    }
+}
