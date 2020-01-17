2cb2c2d09abc0f8a919b9c8d969e53806e9955ae
Ruriko Pang <2419817128@qq.com>
2020-01-17 09:05:05 +0800

    Create 二叉树.101.对称二叉树.java

---
+++ 二叉树.101.对称二叉树.java
@@ -0,0 +1,30 @@
+给定一个二叉树，检查它是否是镜像对称的。
+例如，二叉树 [1,2,2,3,4,4,3] 是对称的。
+    1
+   / \
+  2   2
+ / \ / \
+3  4 4  3
+
+但是下面这个 [1,2,2,null,3,null,3] 则不是镜像对称的:
+    1
+   / \
+  2   2
+   \   \
+   3    3
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/symmetric-tree
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+=====================解法=====================
+解法1 递归法
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        return check(root, root);
+    }
+    public boolean check(TreeNode left, TreeNode right) {
+        if (left == null && right == null) return true;
+        if (left == null || right == null) return false;
+        return left.val == right.val && check(left.left, right.right) && check(left.right, right.left);
+    }
+}
