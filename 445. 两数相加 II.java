45cb1c9233066dc63e84abbacea0296d05c6ef1e
Ruriko Pang <2419817128@qq.com>
2020-04-14 09:43:43 +0800

    Create 445. 两数相加 II.java

---
+++ 445. 两数相加 II.java
@@ -0,0 +1,53 @@
+给你两个 非空 链表来代表两个非负整数。数字最高位位于链表开始位置。它们的每个节点只存储一位数字。将这两数相加会返回一个新的链表。
+你可以假设除了数字 0 之外，这两个数字都不会以零开头。
+
+进阶：
+如果输入链表不能修改该如何处理？换句话说，你不能对列表中的节点进行翻转。
+
+示例：
+
+输入：(7 -> 2 -> 4 -> 3) + (5 -> 6 -> 4)
+输出：7 -> 8 -> 0 -> 7
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/add-two-numbers-ii
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+===============解法======================
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        if (l1 == null)
+            return l2;
+        if (l2 == null)
+            return l1;
+        Stack<Integer> s1 = new Stack<>();
+        Stack<Integer> s2 = new Stack<>();
+        while (l1 != null) {
+            s1.push(l1.val);
+            l1 = l1.next;
+        }
+        while (l2 != null) {
+            s2.push(l2.val);
+            l2 = l2.next;
+        }
+        ListNode head = null;
+        int carry = 0;
+        while (!s1.isEmpty() || !s2.isEmpty() || carry > 0) {
+            int sum = carry;
+            sum += s1.isEmpty() ? 0 : s1.pop();
+            sum += s2.isEmpty() ? 0 : s2.pop();
+            ListNode node = new ListNode(sum % 10);
+            node.next = head;
+            head = node;
+            carry = sum / 10;
+        }
+        return head;
+    }
+}
